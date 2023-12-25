ebddc8c94bedf9a7f724e7b210864b9b4f27d574
KhushiMhasange <99010742+KhushiMhasange@users.noreply.github.com>
2023-12-25 12:29:03 +0530

    Updates

---
+++ precache-manifest.d0fced6c5f589536493bcc326bbf6787.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ef75d6f52cb90c3c232d8751825f73",
+    "url": "/KhushiMhasange/Portfolio.git/index.html"
+  },
+  {
+    "revision": "6dc35a0ddd21b3e412b9",
+    "url": "/KhushiMhasange/Portfolio.git/static/css/2.5e1b3031.chunk.css"
+  },
+  {
+    "revision": "a12f9ddc73c877c2d7d9",
+    "url": "/KhushiMhasange/Portfolio.git/static/css/main.13b9a711.chunk.css"
+  },
+  {
+    "revision": "6dc35a0ddd21b3e412b9",
+    "url": "/KhushiMhasange/Portfolio.git/static/js/2.b5aca96c.chunk.js"
+  },
+  {
+    "revision": "797739ac1b5698394f96",
+    "url": "/KhushiMhasange/Portfolio.git/static/js/3.e339ce8c.chunk.js"
+  },
+  {
+    "revision": "a12f9ddc73c877c2d7d9",
+    "url": "/KhushiMhasange/Portfolio.git/static/js/main.73585613.chunk.js"
+  },
+  {
+    "revision": "14149f3aabc4479b625a",
+    "url": "/KhushiMhasange/Portfolio.git/static/js/runtime~main.586ed1bf.js"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "5f3ca49706055342e92708c7dd31849f",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/banner-bg.5f3ca497.png"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "f59585800e801751a6dbe7cf9a475a70",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/header-img.f5958580.png"
+  },
+  {
+    "revision": "dea05c56b45857a24dcba5a23202ec70",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/meter1.dea05c56.svg"
+  },
+  {
+    "revision": "7fb1b05f49ba5c3b4570e1dec3728213",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/meter2.7fb1b05f.svg"
+  },
+  {
+    "revision": "19e4204daee7b06ccd0aa7807bc37653",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/meter3.19e4204d.svg"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "1fd125719665c7df09fbe0e3df45fc3a",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/nav-icon2.1fd12571.svg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "e6619dbffa52e4b750364ac485742533",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/proj-img1.e6619dbf.png"
+  },
+  {
+    "revision": "ca480045aa6487325981aed15229ab3f",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/project-img2.ca480045.png"
+  },
+  {
+    "revision": "cbd0e129127672cd9f501ce4e019a7af",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/project-img3.cbd0e129.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/KhushiMhasange/Portfolio.git/static/media/revicons.2feb69cc.eot"
+  }
+]);
